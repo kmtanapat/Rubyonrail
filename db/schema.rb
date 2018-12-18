66bee5fe06be0d7f4ad6f2b75b61bed4bcfc2a0b
kmtanapat <kmtanapat>
2018-12-18 16:15:03 +0700

    users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_031756) do
+ActiveRecord::Schema.define(version: 2018_12_18_040723) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
