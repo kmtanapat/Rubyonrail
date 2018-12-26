8b46e5203fe69e59eb63716c2f7843b41f440e34
kmtanapat <kmtanapat>
2018-12-26 17:44:40 +0700

    add customer

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,12 +1,12 @@
 class AreasController < ApplicationController
   def create
     @user = User.find(params[:user_id])
-    @area = @user.comments.create(area_params)
-    redirect_to user_path(@user)
+    @area = @user.areas.create(area_params)
+    redirect_to users_path(@user)
   end
 
   private
   def area_params
-    params.require(:user).permit(:district,:province)
+    params.require(:area).permit(:district, :province)
   end
 end
