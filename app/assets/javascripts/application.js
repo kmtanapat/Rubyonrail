c880a9ba7e7324a7b23b588b09142035525027e6
kmtanapat <kmtanapat>
2019-01-10 17:21:46 +0700

    renovated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,20 +20,3 @@
 //= require pickadate/picker.date
 //= require_tree .
 $('.datepicker').pickadate()
-$(function() {
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-
-            reader.onload = function (e) {
-                $('#img_prev').attr('src', e.target.result);
-            }
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
-
-    $("#avatar-upload").change(function(){
-        $('#img_prev').removeClass('hidden');
-        readURL(this);
-    });
-});
